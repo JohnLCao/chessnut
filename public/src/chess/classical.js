f124ab911aac836ebc449c4182c09d47635a0104
John L. Cao <john_caohaha@hotmail.com>
2017-06-29 17:34:10 -0700

    clean up left over chessboard files, include chess.js, implment classical chess variation without embellishment or promotion to different pieces

--- public/src/chess/classical.js
+++ public/src/chess/classical.js
@@ -1,9 +1,72 @@
 (function() {
-	var cfg = {
-		draggable: true,
-		dropOffBoard: 'snapback',
-		position: 'start'
+	var board;
+	var game = new Chess();
+
+	// do not pick up pieces if the game is over
+	// only pick up pieces for the side to move
+	var onDragStart = function(source, piece, position, orientation) {
+	  if (game.game_over() === true ||
+	      (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+	      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+	    return false;
+	  }
 	};
 
-	var board = ChessBoard('board', cfg);
-})();
+	var onDrop = function(source, target) {
+	  // see if the move is legal
+	  var move = game.move({
+	    from: source,
+	    to: target,
+	    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+	  });
+
+	  // illegal move
+	 if (move === null) return 'snapback';
+
+	  updateStatus();
+	};
+
+	// update the board position after the piece snap 
+	// for castling, en passant, pawn promotion
+	var onSnapEnd = function() {
+	  board.position(game.fen());
+	};
+
+	var updateStatus = function() {
+	  // var status = '';
+	  // var moveColor = (game.turn() === 'w') ? 'While' : 'Black';
+
+	  // checkmate?
+	  if (game.in_checkmate() === true) {
+	    
+	  }
+
+	  // draw?
+	  else if (game.in_draw() === true) {
+	    
+	  }
+
+	  // game still on
+	  else {
+
+	    // check?
+	    if (game.in_check() === true) {
+
+	    }
+	  }
+	};
+
+	var cfg = {
+	  draggable: true,
+	  position: 'start',
+	  onDragStart: onDragStart,
+	  onDrop: onDrop,
+	  onSnapEnd: onSnapEnd
+	};
+
+	// requires div id = board in html
+	board = ChessBoard('board', cfg);
+
+	updateStatus();
+
+})(); //IIFE

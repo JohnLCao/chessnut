8454eaabbe0de9d6bab9f186c0c5da7d3fac9d9e
John L. Cao <john_caohaha@hotmail.com>
2017-07-12 11:28:07 -0700

    fiddle with production pg db config

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170711211335) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.datetime "created_at", null: false

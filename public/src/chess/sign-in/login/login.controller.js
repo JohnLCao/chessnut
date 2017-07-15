ff933b088ae7472aa04bf2deef2679e642867430
John L. Cao <john_caohaha@hotmail.com>
2017-07-15 12:06:44 -0700

    front end login routing proof of concept

--- public/src/chess/sign-in/login/login.controller.js
+++ public/src/chess/sign-in/login/login.controller.js
@@ -5,8 +5,16 @@
 angular.module('chessnut')
 .controller('LoginController', LoginController)
 
-function LoginController(){
+LoginController.$inject = ['$state']
+function LoginController($state){
 	var $ctrl = this;
+
+	$ctrl.login = function(){
+		var loginSuccess = true;
+		if (loginSuccess){
+			$state.transitionTo('classical');
+		}
+	};
 };
 
 })(); //IIFE

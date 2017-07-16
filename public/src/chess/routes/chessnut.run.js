38a25235e5c69252929663de4d2e348931343a41
John L. Cao <john_caohaha@hotmail.com>
2017-07-16 14:02:19 -0700

    clean up some logging statements.

--- public/src/chess/routes/chessnut.run.js
+++ public/src/chess/routes/chessnut.run.js
@@ -10,15 +10,13 @@ function RunFunc($rootScope, LoginService, $transitions){
 	$transitions.onStart({to: stateMatch}, ensureLogin);
 
 	function ensureLogin(trans){
-		console.log('ensureLogin was ran');
 		if (!LoginService.loggedIn){
-			console.log('detected that user has not logged in');
 			return trans.router.stateService.target('login');
 		}
 	};
 
 	function stateMatch(state){
-		console.log(state);
+		// console.log(state);
 		var protected_states = [
 		'classical','blitz','chess960','app-description','john-description','acknowledgements'
 		];

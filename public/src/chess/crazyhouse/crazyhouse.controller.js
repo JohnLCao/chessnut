2bb43af9806628775a0541c532491febeb3a05f0
John L. Cao <john_caohaha@hotmail.com>
2017-07-26 10:31:44 -0700

    clean up some piece an square detection code for crazyhouse drops

--- public/src/chess/crazyhouse/crazyhouse.controller.js
+++ public/src/chess/crazyhouse/crazyhouse.controller.js
@@ -9,14 +9,12 @@ CrazyhouseController.$inject = ['GameService', '$document', 'MoveNavigationServi
 function CrazyhouseController(GameService, $document, MoveNavigationService, $scope, $rootScope){
 	var $ctrl = this;
 	$ctrl.name = "crazyhouse";
-	$ctrl.dragging = false;
 	$ctrl.drag_piece = null;
 
 	$ctrl.initialize = function(){
 		$ctrl.game = GameService.getGame();
 		$ctrl.board = GameService.makeBoard($ctrl.name);
 		$(document).on('keyup', MoveNavigationService.moveListener);
-		$('.square-55d63').on('mouseover', $ctrl.dropHelper);
 	};
 
 	$document.ready($ctrl.initialize);
@@ -30,21 +28,17 @@ function CrazyhouseController(GameService, $document, MoveNavigationService, $sc
 	});
 
 	$scope.$on('crazyhouse:reserves:drag_start', function(event, data){
-		$ctrl.dragging = true;
 		$ctrl.drag_piece = data.piece;
 	})
 
-	$ctrl.onDropReserve = function(){
-		console.log('droped');
-		$rootScope.$broadcast('crazyhouse:reserves:drag_stop', {piece: $ctrl.drag_piece});
-	};
-
-	$ctrl.dropHelper = function(e){
-		if ($ctrl.dragging){
-			console.log('a', $ctrl.drag_piece, 'on', e.target.getAttribute('data-square'));
-		 	$ctrl.dragging = false;
-		 	$ctrl.drag_piece = null;
-		}
+	$ctrl.onDropReserve = function(e){
+		console.log(e.target);
+		console.log('droped a', $ctrl.drag_piece, 'on', e.target.getAttribute('data-square'));
+		$rootScope.$broadcast('crazyhouse:reserves:drag_stop', {
+			piece: $ctrl.drag_piece,
+			square: e.target.getAttribute('data-square')
+		});
+		$ctrl.drag_piece = null;
 	};
 
 	$ctrl.onDragOver = function(e){
